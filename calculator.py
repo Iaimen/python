d35a79606192f83437f720eb84fa38c36b0bde46
Iaimen <145614699+Iaimen@users.noreply.github.com>
2024-03-13 02:20:36 +0600

    calculator added

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# Arthemetic operation using Calculator
+
+print("CALCULATOR")
+a=5
+b=7
+sum=a+b
+print('addition:',sum)
+subtract=a-b
+print('Subtraction:', subtract)
+multiply=a*b
+print('Multiplication:', multiply)
+div=a//b
+print('Division:', div)
+
